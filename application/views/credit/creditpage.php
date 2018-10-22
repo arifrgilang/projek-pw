87fde3dbbafa8728435830ac94ba6ac349919378
RokuojiShoji <fahmi.kems15@gmail.com>
2018-10-22 15:16:09 +0700

    ngerubah credit sama css

--- application/views/credit/creditpage.php
+++ application/views/credit/creditpage.php
@@ -1,27 +1,25 @@
 <!--section about us-->
-<br>
-<br>
-<br>
-<h1 style="text-align:center">The Team</h2>
+<h1 style="text-align:center">The Team</h1>
 <hr>
-<div class="grid-container" id="nameCard">
-
-	<div>
-        <br><br>
-        <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/18.jpg">
-        <br>
-        Felia Sri Indriyani
-    </div>
-	<div>
-        <br><br>
-        <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/30.jpg">
-        <br>
-        Arif Rizky Gilang
-    </div>
-	<div>
-        <br><br>
-        <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/62.jpg">
-        <br>
-        Fahmi Izzuddin
+<div class="menu utama">
+    <div class="grid-container" id="nameCard">
+	    <div>
+            <br><br>
+            <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/18.jpg">
+            <br>
+            Felia Sri Indriyani
+        </div>
+	    <div>
+            <br><br>
+            <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/30.jpg">
+            <br>
+            Arif Rizky Gilang
+        </div>
+	    <div>
+            <br><br>
+            <img class="credit-pic" src="http://hda.himatif.org/assets/foto/2017/62.jpg">
+            <br>
+            Fahmi Izzuddin
+        </div>
     </div>
 </div>
