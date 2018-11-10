250ffb8c2b445eec2410d268ba6d5714add8703d
RokuojiShoji <fahmi.kems15@gmail.com>
2018-11-10 23:58:26 +0700

    benerin checkpage

--- application/views/checkpage/checkpage.php
+++ application/views/checkpage/checkpage.php
@@ -7,6 +7,9 @@
             <div style="font-size: 24px">Kode Pesanan</div>
             <input  id="kodPes" type="text" name="kodPes">
         </form>
+
         <br><br><br><br>
-        <span style="font-size: 20px">status muncul di sini</span>
+        
+        <!-- buat status -->
+        <span id="status" style="font-size: 20px">status muncul di sini</span>
     </div>
