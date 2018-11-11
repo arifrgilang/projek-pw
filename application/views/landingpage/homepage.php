35b92e4a61d5ca4b65d3d389921cb9490e639ce1
feliasya <famsfelia@gmail.com>
2018-11-11 22:11:56 +0700

    testimoni

--- application/views/landingpage/homepage.php
+++ application/views/landingpage/homepage.php
@@ -5,7 +5,8 @@
 	</div>
 
 	<div class="container2">
-		<h2 style="padding-top:25vh">"KNOWLEDGE IS NEVER BE THE LIMIT"</h2>
+		<h2 style="padding-top:25vh">"KNOWLEDGE 
+		IS NEVER BE LIMIT"</h2>
   		<h3>We support you to the next level</h3>
 	
 		<button onclick="smoothScroll(document.getElementById('testi'))" class="button" style="vertical-align:middle"><span> testimoni </span></button>
@@ -13,9 +14,15 @@
 
 	<div id="testi"class="containerTesti">
 		<div>
-			<h1>&nbspHIYA &nbsp HIYA</h1>
+			<h2>"Awalnya lagi bingung nyari buku buat anak terus ketemu sama web ini dan masalahnya kurang percaya juga sama transaksi online gitu, tapi pas udah pake POOKS ternyata transaksi online tuh mudah banget dan kualitas produk jadinya pun ga mengecewakan"</h2>
 		</div>
 		<div>
-			ini testimoni
+			-Shera, ibu rumah tangga
+		</div>
+		<div>
+			<h2>"Aku lagi dikejar-kejar sama skripsi, nah aku tuh pake e-book dari luar gitu dan pas aku cari ternyata ga ada di Indo, karena mata aku tuh capek kalau lama-lama liat laptop makanya aku mau nyari tempat nyetak gitu kan, eh pas lagi nyari-nyari info online ketemu sama web ini. Penasaran dong? terus aku coba dan hasilnya bagus bgt jadi skripsiku bakal cepet selesai deh"</h2>
+		</div>
+		<div>
+			-Marsha, mahasiswa
 		</div>
 	</div>
