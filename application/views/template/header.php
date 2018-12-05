18b54e1c1580202ef25a6238de76ebcb63e2f342
feliasya <famsfelia@gmail.com>
2018-12-05 12:31:57 +0700

    ngubah style header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,18 +10,15 @@
 	<header>
 	<!--navbar-->
 	<nav id="menunav">
-	    <input type="checkbox" id="checkbox1" />
-	    <label for="checkbox1">
 			<ul class="menu bar">
 				<li><a href="<?php echo site_url(); ?>">Home</a></li>
 			    <li><a href="<?php echo site_url('pages/price'); ?>">Price List</a></li>
 			    <li><a href="<?php echo site_url('pages/faq'); ?>">FAQ</a></li>
 				<li><a href="<?php echo site_url('pages/checkpage'); ?>">Order Check</a></li>
 				<li><a href="<?php echo site_url('pages/credit'); ?>">Credit</a></li>
-				<li><a href="<?php echo site_url('pages/orderpage');?>" id="order" style="color: #dbdbdb;">Print E-Book Order</a></li> 
-				<li><a href="<?php echo site_url('login'); ?>">Administrator</a></li>
+				<li style="float:right"><a href="<?php echo site_url('pages/orderpage');?>" id="order" class="active">Print E-Book Order</a></li> 
+				<li style="float:right"><a href="<?php echo site_url('login'); ?>" >Administrator</a></li>
 			</ul>
-		<span class="toggle">☰</span>
 		</label>
 	</nav>
 	</header>
