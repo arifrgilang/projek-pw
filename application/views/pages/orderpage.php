edee0cd0fad12e305a792b388f4a86405ebbd52e
Arif R Gilang <arif17003@mail.unpad.ac.id>
2018-12-06 06:30:25 +0700

    add logic to orderpage

--- application/views/pages/orderpage.php
+++ application/views/pages/orderpage.php
@@ -17,14 +17,13 @@
     <input type="text" name="link_file" placeholder="Masukkan Link File Buku">
     <br>
 </div>
-<div class="kanan">
+<div class="kanan" id="form_buku" onChange="myFunction()">
     <div class="grid-container">
     <label for="ukuran_kertas">Ukuran Kertas</label>
-    <label for="jenis_kertas">Jenis Kertas</label>
     </div>
     <br>
 
-    <select name="ukuran_kertas">
+    <select name="ukuran_kertas" id="uk_kertas">
         <option value="uk_a4">A4</option>
         <option value="uk_a5">A5</option>
         <option value="uk_b4">B4</option>
@@ -32,35 +31,37 @@
     </select>
     <br>
 
-    
-    <label for="jenis_cover">Jenis Cover</label> 
-    <select name="jenis_kertas">
+    <label for="jenis_kertas">Jenis Kertas</label>
+    <select name="jenis_kertas" id="je_kertas">
         <option value="jk_hssd">HSSD</option>
         <option value="jk_hvs">HVS</option>
     </select>
     <br>
 
-    <select name="jenis_cover">
-        <option value="cov_01">Soft Cover</option>
-        <option value="cov_02">A5</option>
+    <label for="jenis_cover">Jenis Cover</label> 
+    <select name="jenis_cover" id="je_cover">
+        <option value="5000">Soft Cover</option>
+        <option value="10000">A5</option>
     </select>
     <br>
 
     <label for="kurir">Kurir</label>
-    <select name="kurir">
-        <option value="k01">JNE</option>
-        <option value="k02">Go-Jek</option>
-        <option value="k03">J&T</option>
-        <option value="k04">Tiki</option>
+    <select name="kurir" id="kurir">
+        <option value="8000">JNE</option>
+        <option value="5000">Go-Jek</option>
+        <option value="6000">J&T</option>
+        <option value="9000">Tiki</option>
     </select>
 
     <label for="jumlah_halaman">Jumlah Halaman</label>
     <input id="hal" type="text" name="jumlah_halaman">
     <br>
 </div>
-
+    
     <button type="submit">Konfirmasi Pembelian</button>
     <br>
+    <label for="harga">Total Harga</label>
+    <h5 id="harga"></h5>
 
     
     
@@ -84,6 +85,42 @@
 </div>
 
 <script>
+    function myFunction() {
+        var je_cov = document.getElementById("je_cover").value;
+        var je_kertas = document.getElementById("je_kertas").value;
+        var uk_kertas = document.getElementById("uk_kertas").value;
+        var kurir = document.getElementById("kurir").value;
+        var hal = document.getElementById("hal").value;
+
+        var hargaKertas = 0;
+        var hargaCover = parseInt(je_cov);
+        var hargaKurir = parseInt(kurir);
+
+        if(je_kertas == "jk_hssd"){
+            if(uk_kertas == "uk_a4"){
+                hargaKertas = 150;
+            } else if (uk_kertas == "uk_a5"){
+                hargaKertas = 80;
+            } else if (uk_kertas == "uk_b4"){
+                hargaKertas = 150;
+            } else if (uk_kertas == "uk_b5"){
+                hargaKertas = 80;
+            }
+        } else if (je_kertas == "jk_hvs"){
+            if(uk_kertas == "uk_a4"){
+                hargaKertas = 110;
+            } else if (uk_kertas == "uk_a5"){
+                hargaKertas = 60;
+            } else if (uk_kertas == "uk_b4"){
+                hargaKertas = 110;
+            } else if (uk_kertas == "uk_b5"){
+                hargaKertas = 60;
+            }
+        }
+        var totalHarga = (parseInt(hal) * hargaKertas) + hargaCover + hargaKurir;
+        // document.getElementById("harga").innerHTML = je_cov + je_kertas + uk_kertas + kurir + hal;
+        document.getElementById("harga").innerHTML = parseInt(hal) * hargaKertas + hargaCover + hargaKurir  ;
+    }
 // Get the modal
 var modal = document.getElementById('myModal');
 
