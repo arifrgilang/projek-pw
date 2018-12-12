94dbaccffc233b90dcd0da6bab309f35ae85916f
Arif R Gilang <arif17003@mail.unpad.ac.id>
2018-12-12 09:39:53 +0700

    fix bugs

--- application/views/pages/order.php
+++ application/views/pages/order.php
@@ -8,6 +8,7 @@
     <?php endforeach; ?>
 
     <hr>
+    <h3>Kirimkan bukti pembayaran ke Whatsapp : 083821666100 A.N Arif R Gilang</h3>
     <?php foreach ($dataorder as $v): ?>
         <h3>Status pesanan anda bisa di cek di pada halaman Order Check dengan kode:</h3>
         <h1><strong><?php echo $v?></strong> </h1>
